94550bc85812d058da91611ecaa7e53a0bee3300
pemtash <70712789+pemtash@users.noreply.github.com>
2021-03-23 15:30:38 +0530

    Add files via upload

---
+++ deliveryTime.R
@@ -0,0 +1,83 @@
+delivery_time <- read.csv("C:\\Users\\user\\Desktop\\EXCELR ASSIGNMENT\\ASSIGNMENT\\assignment4 simple_linear_regression\\delivery_time.csv")
+View(delivery_time)
+attach(delivery_time)
+
+# EXPLORATORY DATA ANALYSIS
+summary(delivery_time)
+
+# GRAPHICAL EXPLORATION
+boxplot(Sorting.Time, col = "dodgerblue4")
+
+hist(Delivery.Time)
+hist(Sorting.Time)
+
+qqnorm(Delivery.Time)
+qqline(Delivery.Time)
+
+qqnorm(Delivery.Time)
+qqline(Sorting.Time)
+
+#scatter plot
+plot(Delivery.Time,Sorting.Time)
+cor(Delivery.Time,Sorting.Time)
+
+reg <- lm(Delivery.Time~Sorting.Time) # Y ~ X
+summary(reg)
+
+confint(reg,level=0.95)# confidence interval at 95%
+
+pred <- predict(reg,interval="predict")
+
+pred <- as.data.frame(pred)
+
+print(pred)
+View(pred)
+
+# transform the variables to check whether the predicted values are better
+reg_sqrt <- lm(Delivery.Time~sqrt(Sorting.Time))
+summary(reg_sqrt)
+
+confint(reg_sqrt,level=0.95)
+predict(reg_sqrt,interval="predict")
+pred1 <- predict(reg_sqrt,interval="predict")
+pred1
+pred1 <- as.data.frame(pred1)
+
+reg_log<-lm(Delivery.Time~log(Sorting.Time))
+summary(reg_log)
+
+
+confint(reg_log,level=0.95)
+predict(reg_log,interval="predict")
+pred2 <- predict(reg_log,interval="predict")
+pred2 <- as.data.frame(pred2)
+pred2
+View(pred2)
+wc.at <- read.csv("C:\\Users\\user\\Desktop\\EXCELR ASSIGNMENT\\ASSIGNMENT\\assignment4 simple_linear_regression\\wc-at.csv")
+cor(pred2$fit, wc.at$AT)
+
+
+reg1<-lm(log(AT)~Waist + I(Waist*Waist), data=wc.at)
+summary(reg1)
+confint(reg1,level=0.95)
+predict(reg1,interval="predict")
+pred3<-predict(reg1,interval="predict")
+pred3<-as.data.frame(pred)
+pred3
+View(pred3)
+
+exp(pred3$fit)
+cor(exp(pred$fit),wc.at$AT)
+
+reg_sqrt1<-lm(sqrt(AT)~Waist, data=wc.at)
+summary(reg_sqrt1)
+confint(reg_sqrt1,level=0.95)
+predict(reg_sqrt1,interval="predict")
+
+
+reg_log1<-lm(log(AT)~Waist, data=wc.at)
+summary(reg_log1)
+confint(reg_log1,level=0.95)
+predict(reg_log1,interval="predict")
+
+
